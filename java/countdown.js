7a17e4197e0de2c7382c48c981e0f9c4222fac42
dcorney1 <87706212+dcorney1@users.noreply.github.com>
2021-11-27 08:35:04 -0500

    Revert "Revert "fixed timer""
    This reverts commit 5f54948c00c8754995d270b8d22f8c7110d2724c.

--- java/countdown.js
+++ java/countdown.js
@@ -1,10 +1,14 @@
 function countdown() {
-var date = new Date("August 27, 2022");
+var date = new Date("August 28, 2022");
 var now = new Date();
 var diff = date.getTime() - now.getTime();
 var daysLeft = Math.floor(diff / (1000 * 60 * 60 * 24));
   if (daysLeft > 1) {
-  document.getElementById("weddingCountdown").innerText= `${daysLeft} days left!`;
+    if (daysLeft == 365){
+      document.getElementById("weddingCountdown").innerText= `We're celebrating our -1!`;
+    } else {
+      document.getElementById("weddingCountdown").innerText= `${daysLeft} days left!`;
+    }  
 } else if (daysLeft > 0) {
 document.getElementById("weddingCountdown").innerText= `${daysLeft} day left!`;
 } else if
