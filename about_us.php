b6d38f934037c2318d5159ba951c77542d29221a
dcorney1 <87706212+dcorney1@users.noreply.github.com>
2021-11-27 08:14:12 -0500

    This is testing the current issues I am facing

--- about_us.php
+++ about_us.php
@@ -5,11 +5,36 @@
   <?php
     include_once 'nav.php'
   ?>
-  <div class="text-box">
-      <h1>Coming Soon</h1>
-    </div>
-      <script src="java/countdown.js"></script>
-      <script src="java/menu.js"></script>
-    </section>
-<!-- Travel Content -->
-<!-- Footer -->
+<div class="text-box">
+    <h1>Coming Soon</h1>
+</div>
+</section>
+<section class="travel">
+<div class="carousel-container">
+  <i class="fa fa-arrow-left"  id="prevBtn" aria-hidden="true"></i>
+  <i class="fa fa-arrow-right" id="nextBtn" aria-hidden="true"></i>
+  <div class="carousel-slide">
+    <?php
+      $directory = './images/carousel';
+      $images = glob($directory . "/*.png");
+      // print_r($images);
+      echo '<div id="lastClone">
+        <img src="' . end($images) . '" alt=""></div>';
+      foreach($images as $image)
+      {
+        {
+         echo '<div>
+           <img src="' . $image . '" alt=""></div>';
+       }
+      }
+      echo '<div id="firstClone">
+        <img src="' . $images[0] . '" alt=""></div>';
+    ?>
+  </div>
+</div>
+</section>
+
+
+<?php
+  include_once 'footer.php'
+?>
