102c5ba8cfb5247e9c542bec02129ae124c53716
dcorney1 <87706212+dcorney1@users.noreply.github.com>
2021-11-27 08:14:12 -0500

    This is testing the current issues I am facing

---
+++ java/accordion.js
@@ -0,0 +1,29 @@
+<style>
+.answer {
+  padding: 0 18px;
+  background-color: white;
+  max-height: 0;
+  overflow: hidden;
+  transition: max-height 0.2s ease-out;
+}
+</style>
+
+
+var acc = document.getElementsByClassName("accordion-item");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
