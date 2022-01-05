c3cdd83cb17182b8a646f9863031b43bf5e3249f
n1530397 <daniel.corney@lmi.com>
2022-01-05 08:17:23 -0700

    no message

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -1,7 +1,6 @@
 <?php
   //defin PDO - tell about the database file
-  $dir = 'sqlite:' . realpath(__DIR__ . '/..') . './db/wedding.sqlite';
-  print($dir);
+  $dir = 'sqlite:' . realpath(__DIR__ . '/..') . '/db/wedding.sqlite';
   $dbh  = new PDO($dir) or die("cannot open the database");
   $result = $dbh->exec('PRAGMA journal_mode=WAL;');
   if ($result === FALSE) {
