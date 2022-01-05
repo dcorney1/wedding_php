8f0c234b6c11e84706133a80011be6f9f0cafed8
n1530397 <daniel.corney@lmi.com>
2022-01-05 11:34:26 -0700

    yeet

--- nav.php
+++ nav.php
@@ -8,7 +8,7 @@
       <li><a href="events.php">Events</a></li>
       <li><a href="photos.php">Photos</a></li>
       <!-- <br id="menu_photo">Home -->
-      <li><a href="index.php" id="top"><img src="images/logo.png" id="menu_photo"></a></li>
+      <li id="menu_photo"><a href="index.php" id="top"><img src="images/logo.png"></a></li>
       <li><a href="faq.php">FAQ</a></li>
       <li><a href="registry.php">Registry</a></li>
       <li><a href="rsvp.php">RSVP</a></li>
