965f9013d5ab538bcbf41cd73c2830529bf8a949
n1530397 <daniel.corney@lmi.com>
2022-01-05 14:15:57 -0700

    no message

--- travel.php
+++ travel.php
@@ -14,7 +14,7 @@
   <div class = "travel-container">
   <div class="date-header">
     <h1>Where to Stay</h1>
-  </div>
+  
         <h2>The Essex, Vermont's Culinary Resort & Spa</h2>
         <h3>70 Essex Way, Essex Junction, VT 05452</h3>
         <p>The weekend's events will take place here. Transportation to and from the ceremony and reception will be provided from The Essex. Use the link below to reserve a room in our block.</p>
@@ -22,9 +22,10 @@
         <p><br>(Note: More room options are available by clicking "View More Rooms")</p>
         <p><br></p>
         <a href="https://be.synxis.com/?adult=1&arrive=2022-08-26&chain=21123&child=0&currency=USD&depart=2022-08-28&group=3807KL&hotel=34805&level=hotel&locale=en-US&rooms=1" class="hero-btn red-btn" target="_blank">Book Now</a>
-  <div class="date-header">
+        </div>
+        <div class="date-header">
       <h1>Getting There</h1>
-  </div>
+  
   <div class="row">
       <div class="travel-col">
         <h2><i class="fa fa-plane" aria-hidden="true"></i>  Flying  <i class="fa fa-plane" aria-hidden="true"></i></h2>
@@ -38,23 +39,47 @@
         <p>The Essex is a 3 hour and 15 minute drive from Boston, so flying into Boston Logan International Airport (BOS) is also an option.<br><br>There is free onsite parking at The Essex.</p>
       </div>
   </div>
+  </div>
   <div class="date-header">
         <h1>Things to do</h1>
         <h2>Some of our favorite activities in the area!</h2>
-  </div>
+  
   <div class="row">
   <div class="travel-col">
         <h2><i class="fa fa-cutlery" aria-hidden="true"></i>  Dine  <i class="fa fa-glass" aria-hidden="true"></i></h2>
-        <h3>Burlington International Airport (BVT)</h3>
-        <p>The closest airport is Burlington International Airport (BVT). It is a 15 minute drive from The Essex. The Essex has shuttles that will transport guests to and from the airport. Call the front desk to arrange airport transportation. Uber/Lyft are also available in the area.</p>
+        <h3>Eat</h3>
+        <ul>
+          <li><a href="http://henofthewood.com/" target="_blank">Hen of the Wood</a></li>
+          <li><a href="https://www.farmhousetg.com/" target="_blank">The Farmhouse Tap and Gril</a></li>
+          <li><a href="http://www.burlingtonwhiskeyroom.com/" target="_blank">The Whiskey Room</a></li>
+        </ul>
+        <h3>Drink</h3>
+        <ul>
+          <li><a href="http://www.foambrewers.com/" target="_blank">Foam Brewers</a></li>
+          <li><a href="http://www.monarchandthemilkweed.com/" target="_blank">Monarch and the Milkweed</a></li>
+          <li><a href="http://www.vermontdistillers.com/" target="_blank">Smugglers' Notch Distillery</a></li>
+        </ul>
       </div>
       <div class="divider">
       </div>
       <div class="travel-col">
-        <h2><i class="fa fa-bicycle" aria-hidden="true"></i>  Explore  <i class="fa fa-futbol-o" aria-hidden="true"></i></h3>
-        <p>The Essex is a 3 hour and 15 minute drive from Boston, so flying into Boston Logan International Airport (BOS) is also an option.<br><br>There is free onsite parking at The Essex.</p>
+        <h2><i class="fa fa-bicycle" aria-hidden="true"></i>  Explore  <i class="fa fa-futbol-o" aria-hidden="true"></i></h2>
+        <h3>Indoor</h3>
+        <ul>
+          <li><a href="https://burlyaxe.com/?_ga-ft=1XrWCs.0.0.0.0.1Ko920u-1P1Ca8t.0.1" target="_blank">Burly Axe Throwing</a></li>
+          <li><a href="https://www.petracliffs.com/?_ga-ft=1XrWDD.0.0.0.0.1Ko920u-1P1Ca8t.0.1" target="_blank">Petra Cliffs Rock Climbing</a></li>
+          <li><a href="https://www.echovermont.org/?_ga-ft=1XrWEo.0.0.0.0.1Ko920u-1P1Ca8t.0.1" target="_blank">Echo Lake Aquarium</a></li>
+        </ul>
+        <h3>Outdoor</h3>
+        <ul>
+          <li><a href="https://www.stowe.com/explore-the-resort/activities-and-events/hiking.aspx?_ga-ft=1XrWFI.0.0.0.0.1Ko920u-1P1Ca8t.0.1" target="_blank">Hiking Mt. Mannsfield</a></li>
+          <li><a href="https://paddlesurfchamplain.com/?_ga-ft=1XrWGD.0.0.0.0.1Ko920u-1P1Ca8t.0.1" target="_blank">Paddleboard Lake Champlain</a></li>
+          <li><a href="https://www.trailfinder.info/trails/trail/winooski-river-walk" target="_blank">Winooski River Walk</a></li>
+        </ul>
       </div>
   </div>
+  </div>
+  <h2>Find more suggestions on our interactive map!</h2>
         
 </div>
 </section>
