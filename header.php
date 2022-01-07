888e4763a4146f61d467a75d57dbef4f77726b23
n1530397 <daniel.corney@lmi.com>
2022-01-07 10:42:57 -0700

    Yeet! Maybe this will fix the viewport

--- header.php
+++ header.php
@@ -12,6 +12,7 @@
   <link rel="preconnect" href="https://fonts.gstatic.com">
   <!-- <link href=<google font link> rel="stylesheet"> -->
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+  <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
 </head>
 <body onload="countdown()">
 <button onclick="topFunction()" id="topBtn" title="Go to top"><i class="fa fa-arrow-circle-up" aria-hidden="true"></i></button>
