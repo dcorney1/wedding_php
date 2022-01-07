43682169f8f7c2216477f5fd37d9096396913320
n1530397 <daniel.corney@lmi.com>
2022-01-07 10:49:12 -0700

    no message

--- header.php
+++ header.php
@@ -5,14 +5,14 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <meta name="viewport" content="width=device-width" initial-scale=1.0>
+  <!-- <meta name="viewport" content="width=device-width" initial-scale=1.0> -->
   <title>Maddie and Dan's Wedding Website</title>
   <link rel="icon" type="image/png" href="images/logo.png"/>
   <link rel="stylesheet" href="style.css">
   <link rel="preconnect" href="https://fonts.gstatic.com">
   <!-- <link href=<google font link> rel="stylesheet"> -->
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-  <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
+  <meta name="viewport" content="width=device-width", initial-scale=1, minimum-scale=1, maximum-scale=1">
 </head>
 <body onload="countdown()">
 <button onclick="topFunction()" id="topBtn" title="Go to top"><i class="fa fa-arrow-circle-up" aria-hidden="true"></i></button>
