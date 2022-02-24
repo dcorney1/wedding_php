df0bed9855903725aeb41d377a53709f4d44b6bf
n1530397 <daniel.corney@lmi.com>
2022-02-24 12:56:27 -0700

    no message

--- rsvp.php
+++ rsvp.php
@@ -10,6 +10,16 @@
 </div>
 </section>
 <section class="filler">
+
+<div class="date-header">
+    <h1>Currently Unavailable</h1>
+    <div class="row">
+        <!-- <div class="travel-col"> -->
+        <p>Please check back after you receive your invitation!</p>
+
+      </div>
+  </div>
+  
 </section>
  <!-- <section>
   <div class="">
