b37b66ac666e6110f50f4d2f10b68b3b2c6170c1
dcorney1 <87706212+dcorney1@users.noreply.github.com>
2022-05-14 08:35:58 -0400

    Adding changes to include a gift option

--- registry.php
+++ registry.php
@@ -9,7 +9,14 @@
     <h1>Registry</h1>
 </div>
 </section>
-  <section>
+  <section class="travel">
+  <div class = "travel-container">
+  <div class="date-header">
+  <h3>Dear friends and family:</h3>
+  <p>Our registry is small because we have big things planned! We intend to move to Europe for an extended honeymoon, and wanted to make sure we can pack light. Although your presence is enough, if you would like to suggest an idea for our travels and/or make a monetary contribution, it would go a long way to make our trip amazing.</p>
+</div>
+</div>
+
 <script id='script_myregistry_giftlist_iframe' type='text/javascript' src='//www.myregistry.com//Visitors/GiftList/iFrames/EmbedRegistry.ashx?r=Ar0sdoRhz8K1iha6yRpXNg2&v=2'></script>
 <!-- <script id='script_myregistry_giftlist_iframe' type='text/javascript' src='java/registry.js'></script> -->
 <script type="text/javascript">
